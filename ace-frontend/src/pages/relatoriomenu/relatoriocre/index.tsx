f99568604fd2c7d05b00e040123633e2eeb89f1e
caio <caiovitordias1@gmail.com>
2023-04-20 17:52:23 -0300

    integracao das paginas de relatorio

--- ace-frontend/src/pages/relatoriomenu/relatoriocre/index.tsx
+++ ace-frontend/src/pages/relatoriomenu/relatoriocre/index.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { ReactNode, useState } from 'react';
 import Header from '../../../components/header/index'
+import { api } from '../../../service/api';
 import './styles.css'
 
 
 const RelatorioCre: React.FC = () => {
+    
+    interface Parcela{
+        id: number,
+	    idCliente: number,
+        nomeCliente: string,
+	    numeroParcela: number,
+	    dataVencimento: ReactNode,
+	    dataPagamento: ReactNode,
+	    dataCredito: ReactNode,
+	    valorParcela: number,
+	    valorPago: number
+    }
+    
+    const [dataInicio, setDataInicio] = useState("")
+    const [dataFinal, setDataFinal] = useState("")
+    
+    //listaParcela é uma lista de objetos
+    //cada objeto da listaParcela é uma parcela buscada
+    const [listaParcela, setListaParcela] = useState([])
+
+    function fazerBusca(e: React.MouseEvent<HTMLButtonElement>){
+        e.preventDefault()
+
+        api.get(`/Parcela/buscarParcelas/pagamento/${dataInicio}/${dataFinal}`)
+        .then(response => {
+            const resposta = response.data
+            console.log(resposta)
+            setListaParcela(resposta)
+        })
+    }
+    
+    
     return (
         <>
             <Header />
@@ -14,12 +47,14 @@ const RelatorioCre: React.FC = () => {
             <div className="boxDate">
                 <div className='row'>
                     <span>Data de Início: </span>
-                    <input type="date" />
+                    <input type="date" onChange={(e) => setDataInicio(e.target.value)}/>
                 </div>
                 <div className='row'>
                     <span>Data de Término: </span>
-                    <input type="date" />
+                    <input type="date" onChange={(e) => setDataFinal(e.target.value)}/>
                 </div>
+
+                <button onClick={fazerBusca}>Buscar</button>
             </div>
 
             <div className='CenterDetails'>
--- ace-frontend/src/pages/relatoriomenu/relatoriopag/index.tsx
+++ ace-frontend/src/pages/relatoriomenu/relatoriopag/index.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { ReactNode, useState } from 'react';
 import Header from '../../../components/header/index'
+import { api } from '../../../service/api';
 import './styles.css'
 
 
 const RelatorioPag: React.FC = () => {
 
+    interface Parcela{
+        id: number,
+	    idCliente: number,
+        nomeCliente: string,
+	    numeroParcela: number,
+	    dataVencimento: ReactNode,
+	    dataPagamento: ReactNode,
+	    dataCredito: ReactNode,
+	    valorParcela: number,
+	    valorPago: number
+    }
+    
+    const [dataInicio, setDataInicio] = useState("")
+    const [dataFinal, setDataFinal] = useState("")
+    
+    //listaParcela é uma lista de objetos
+    //cada objeto da listaParcela é uma parcela buscada
+    const [listaParcela, setListaParcela] = useState([])
+
+    function fazerBusca(e: React.MouseEvent<HTMLButtonElement>){
+        e.preventDefault()
+
+        api.get(`/Parcela/buscarParcelas/pagamento/${dataInicio}/${dataFinal}`)
+        .then(response => {
+            const resposta = response.data
+            console.log(resposta)
+            setListaParcela(resposta)
+        })
+    }
 
     return (
         <>
@@ -16,19 +46,59 @@ const RelatorioPag: React.FC = () => {
             <div className="boxDate">
                 <div className='row'>
                     <span>Data de Início: </span>
-                    <input type="date" />
+                    <input type="date" onChange={(e) => setDataInicio(e.target.value)}/>
                 </div>
                 <div className='row'>
                     <span>Data de Término: </span>
-                    <input type="date" />
+                    <input type="date" onChange={(e) => setDataFinal(e.target.value)}/>
                 </div>
+
+                <button onClick={fazerBusca}>Buscar</button>
             </div>
 
             <div className='CenterDetails'>
-                <div className="DetailsDate">
+                <table>
+                                <thead>
+                                    <th>id do Cliente</th>
+                                    <th>Data de </th>
+                                    <th>Valor da Parcela</th>
+                                    <th>Valor Pago</th>
+                                </thead>
+                                <tbody>
+                                    {listaParcela.map((result:Parcela, index) =>(
+                                        
+                                        <tr>
+                                            <td key={index}>{result.nomeCliente}</td>
+                                            <td key={index}>{result.dataVencimento}</td>
+                                            <td key={index}>{result.valorParcela}</td>
+                                            <td key={index}>{result.valorPago}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                
+                {/* <div className="DetailsDate">
                     <details>
                         <summary> Nome do Cliente </summary>
-                            <p>informações do clientinho</p>
+                            <table>
+                                <thead>
+                                    <th>id do Cliente</th>
+                                    <th>Data de </th>
+                                    <th>Valor da Parcela</th>
+                                    <th>Valor Pago</th>
+                                </thead>
+                                <tbody>
+                                    {listaParcela.map((result:Parcela, index) =>(
+                                        
+                                        <tr>
+                                            <td key={index}>{result.idCliente}</td>
+                                            <td key={index}>{result.dataVencimento}</td>
+                                            <td key={index}>{result.valorParcela}</td>
+                                            <td key={index}>{result.valorPago}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                     </details>
                     <details>
                         <summary> Nome do Cliente </summary>
@@ -37,8 +107,8 @@ const RelatorioPag: React.FC = () => {
                     <details>
                         <summary> Nome do Cliente </summary>
                             <p>informações do clientinho3</p>
-                    </details>
-                </div>
+                    </details> 
+                </div> */}
             </div>
 
 
