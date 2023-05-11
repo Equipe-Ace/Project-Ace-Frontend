ecee38e033880a7f3a504aa67515b2e54b875245
Ryan Alves <Ryan.queen246@gmail.com>
2023-05-11 08:17:17 -0300

    adicionando o input radio sem css

--- ace-frontend/src/components/FormADM/index.tsx
+++ ace-frontend/src/components/FormADM/index.tsx
@@ -4,36 +4,46 @@ import Dropdown from '../dropdown';
 import BotaoAvancar from '../botaoAvan';
 import { Link } from 'react-router-dom';
 
-const OpcDrop: React.FC = () => {
-  const options = ['Financeiro', 'Comercial'];
-
-  return <Dropdown options={options} />;
-};
 
 
 
-const FormADM: React.FC = () =>{
-    return(
+const FormADM: React.FC = () => {
+    return (
         <>
-        
+
             <div className="bgformadm" >
-            <h1> Cadastro de usuários</h1>
+                <h1> Cadastro de usuários</h1>
                 <div className="inputBoxformadm">
                     <input type="text" required />
                     <span>Email</span>
                 </div>
                 <div className="inputBoxformadm">
-                    <input type="password" required/>
+                    <input type="password" required />
                     <span>senha</span>
                 </div>
-                <OpcDrop />
-                
-            <div className="containerbuttonAVC">
-                <button className="btn btn1"> Avançar ▸</button>
-            </div>
+
+                <div className='rolebox' >
+                    <div>
+                        <span>Comercial</span>
+                        <input type='radio' className='comercial'>
+                        </input>
+
+                        <span>Financeiro</span>
+                        <input type='radio' className='financeiro'>
+                        </input>
+                    </div>
+
+
+
+
+
+                </div>
+                <div className="containerbuttonAVC">
+                    <button className="btn btn1"> Avançar ▸</button>
+                </div>
 
             </div>
-            
+
         </>
     );
 }
