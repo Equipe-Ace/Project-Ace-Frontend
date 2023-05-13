14a4acb8921dde64ac2f3f772679c6ca426b053f
JulioPm142 <juliopm142@gmail.com>
2023-05-12 21:06:28 -0300

    Adicao do campo detalhes no modal da listagem dos clientes, mostrando os detalhes de cada parcela, além de correcao em formatacao do botao de avanco da listagem

--- ace-frontend/src/components/TitReceber/index.tsx
+++ ace-frontend/src/components/TitReceber/index.tsx
@@ -41,9 +41,11 @@ const SelectCli: React.FC = () => {
     const [ParcelasPagas, setParcelasPagas] = useState(0);
     const [ParcelasPendentes,setParcelasPendentes] = useState(0);
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [todasParcelas, setTodasParcelas] = useState(false);
+    const [todasParcelas, setTodasParcelas] = useState(Object);
     const [valorParcela, setValorParcela] = useState(0);
     const [valorServio, setValorServico] = useState(0);
+    const [showText, setShowText] = useState(false);
+    const [showText2, setShowText2] = useState(false);
     
 
 
@@ -116,21 +118,74 @@ const SelectCli: React.FC = () => {
         return () => handleClick(id);
       }
 
+      const handleDropdown = () => {
+        setShowText(!showText);
+      }
+
+      const handleDropdown2 = () => {
+        setShowText2(!showText2);
+      }
     return (
         <>
 
-                <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+<Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+                    
                     <div className='alignRight'>
                         <a className='button-close' onClick={handleCloseModal}>X</a>
                     </div>
-                    <tr><td>Cpf</td> <td>{CPF}</td></tr>
-                    <tr><td>Nome</td> <td>{nome}</td></tr>
-                    <tr><td>Id</td> <td>{id}</td></tr>
-                    <tr><td>Total de Parcelas</td> <td>{TotalParcelas}</td></tr>
-                    <tr><td>Valor das Parcelas</td><td>R$:{valorParcela.toFixed(2)}</td></tr>
-                    <tr><td>Parcelas Pendentes</td><td>{ParcelasPendentes}</td></tr>
-                    <tr><td>Parcelas Pagas</td><td>{ParcelasPagas}</td></tr>
-                    <tr><td>Valor Total</td><td>R$:{valorServio.toFixed(2)}</td></tr>
+                    <div className='arruma'>
+                    <table className='drodownTable'>
+                            <tbody>
+                            <tr><td>Cpf</td> <td>{CPF}</td></tr>
+                            <tr><td>Nome</td> <td>{nome}</td></tr>
+                            <tr><td>Valor Total</td><td>R$:{valorServio.toFixed(2)}</td></tr>
+                            </tbody>
+                        </table>
+                        <br/></div><br/>
+                        <div><h4 className='dropdown' onClick={handleDropdown2}>Dados Gerais</h4>
+                        {showText2 && <p className=''>
+                            <div className='arruma'>
+                            <table className='drodownTable'>                             
+                            <tr><td>Total de Parcelas</td> <td>{TotalParcelas}</td></tr>
+                            <tr><td>Valor das Parcelas</td><td>R$:{valorParcela.toFixed(2)}</td></tr>
+                            <tr><td>Parcelas Pendentes</td><td>{ParcelasPendentes}</td></tr>
+                            <tr><td>Parcelas Pagas</td><td>{ParcelasPagas}</td></tr></table></div></p>}
+                        </div>
+                    <br/>
+                    <div><h4 className='dropdown' onClick={handleDropdown}>Detalhes</h4>
+                        {showText && <p className='alignCenter'> 
+                        { <tbody className='drodownTable'>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Valor Total</th>
+                                <th>Valor Pago</th>
+                                <th>data de Vencimento</th>
+                                <th>data de Pagamento</th>          
+                                <th>data de Crédito</th>
+                            </tr>
+                        </thead>    
+                                    
+                        {todasParcelas.map((item: { id: number, numeroParcela: string, valorPago: Number, valorParcela: Number, dataVencimento:string,dataPagamento:string,dataCredito:string }, id: number) => {
+                            const formattedDataVencimento = new Date(item.dataVencimento).toLocaleDateString('pt-BR');
+                            const formattedDataPagamento = new Date(item.dataVencimento).toLocaleDateString('pt-BR');
+                            const formattedDataCredito = new Date(item.dataVencimento).toLocaleDateString('pt-BR');
+                            return (
+                                <tr key={id}>
+                                    <td>{item.numeroParcela}.</td>  
+                                    <td>R$:{item.valorParcela.toFixed(2)}</td>
+                                    <td>R$:{item.valorPago.toFixed(2)}</td>
+                                    <td>{formattedDataVencimento}</td>
+                                    <td>{ item.dataPagamento ? formattedDataPagamento : "Pendente"}</td>
+                                    <td>{item.dataCredito ? formattedDataCredito : "Pendente"}</td>
+                                </tr>
+                                )   
+                        })}
+                    </tbody> } </p>}
+                    </div>
+
+
+
                 </Modal>
 
             <div className="bgSelect" >
@@ -179,8 +234,9 @@ const SelectCli: React.FC = () => {
                                             setNome(item.nome)
                                             setTotalParcelas(item.servico["parcelas"].length)
                                             Parcelas(item.servico["parcelas"]);
+                                            setTodasParcelas(item.servico["parcelas"])
                                             setValorParcela(item.servico["parcelas"][1]['valorParcela'])
-                                            setValorServico(item.servico['preco'])
+                                            setValorServico(item.servico['preco'])                                          
                                             handleOpenModal();
                                         }}>
                                         <img src={editPen} />
@@ -193,13 +249,14 @@ const SelectCli: React.FC = () => {
                                 )
                             )
                         })}
-                                <td colSpan={4}>
-                                <div className='arruma'>
-                                    <a className='button-1'> <img src={back} alt="botão de edição" onClick={handlePrevPageClick}/></a>
-                                    {page+1}
-                                    <a className='button-1'> <img src={next} alt="botão de edição"  onClick={handleNextPageClick} /></a>
+                                <div className='pageButtom'>
+                                    <div className='arruma'>
+                                        <a className='button-1'> <img src={back} alt="botão de edição" onClick={handlePrevPageClick}/></a>
+                                        {page+1}
+                                        <a className='button-1'> <img src={next} alt="botão de edição"  onClick={handleNextPageClick} /></a>
+                                    </div>
                                 </div>
-                            </td>
+                            
                     </tbody> } 
                 </div>
             </div>
